9f0c9ff7faf0fd23c5e709421000a4d4def6785b
emiliobresolin <emiliotdm@hotmail.com>
2022-08-04 05:55:49 -0300

    basic struct of calculator

---
+++ RestWithAspNetUdemy/RestWithAspNetUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNetUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) & IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDeciaml(firstNumber) + ConvertToDeciaml(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private int ConvertToDeciaml(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
