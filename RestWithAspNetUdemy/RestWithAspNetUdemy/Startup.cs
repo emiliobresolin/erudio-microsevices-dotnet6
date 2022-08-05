80f20c0005f1ad16b2f05c91d92d9d2fd7b521a0
emiliobresolin <emiliotdm@hotmail.com>
2022-08-05 05:57:46 -0300

    implementation of methods get put create and delete and testing with postman

--- RestWithAspNetUdemy/RestWithAspNetUdemy/Startup.cs
+++ RestWithAspNetUdemy/RestWithAspNetUdemy/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithAspNetUdemy.Services;
+using RestWithAspNetUdemy.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +29,8 @@ namespace RestWithAspNetUdemy
         {
 
             services.AddControllers();
+            //depency injection
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
